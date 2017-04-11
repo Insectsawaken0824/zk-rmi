736aafc09c705482717fc6b07296d1e5793e4335
Insectsawaken0824 <18630468824@163.com>
2017-04-11 13:25:56 +0800

    rmi done

--- src/common/Constant.java
+++ src/common/Constant.java
@@ -6,4 +6,8 @@ package common;
 public class Constant {
     public static final String RMI_URL = "rmi://localhost:1098/demo.rmi";
     public static final int RMI_PORT = 1098;
+    public static final String ZK_CONNECTION_STRING = "iz2ze0zkx5pckai7smgnv2z:2181,iz2ze0zkx5pckai7smgnuxz:2181,iz2ze0zkx5pckai7smgnuzz:2181";
+    public static final int ZK_SESSION_TIMEOUT = 5000;
+    public static final String ZK_REGISTRY_PATH = "/registry";
+    public static final String ZK_PROVIDER_PATH = ZK_REGISTRY_PATH + "/provider";
 }
--- src/server/RmiServerZK.java
+++ src/server/RmiServerZK.java
@@ -4,5 +4,7 @@ package server;
  * Created by zhao on 2017/4/11.
  */
 public class RmiServerZK {
-    
+    public static void main(String[] args) {
+
+    }
 }
